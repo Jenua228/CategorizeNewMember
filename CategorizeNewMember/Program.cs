9d3ebd0a1b3207204c765da9ea63991c950a155e
Jenua228 <120947659+Jenua228@users.noreply.github.com>
2024-03-24 14:21:25 +0300

    Add files via upload

---
+++ CategorizeNewMember/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CategorizeNewMember
+{
+    internal class Program
+    {
+        public static List<string> OpenOrSenior(int[][] data)
+        {
+            List<string> result = new List<string>();
+            for (int i = 0; i < data.Length; i++)
+            {
+                if (data[i][0] >= 55 && data[i][1] > 7)
+                {
+                    result.Add("Senior");
+                }
+                else
+                {
+                    result.Add("Open");
+                }
+            }
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            int[][] data = new[] { new[] { 45, 12 }, new[] { 55, 21 }, new[] { 19, 2 }, new[] { 104, 20 } };
+            List<string> result = OpenOrSenior(data);
+        }
+    }
+}
+
+/*public class Kata           //крутое решение чела
+{
+    public static IEnumerable<string> OpenOrSenior(int[][] data)
+    {
+        return data.Select(x => x[0] >= 55 && x[1] > 7 ? "Senior" : "Open");
+    }
+}*/
